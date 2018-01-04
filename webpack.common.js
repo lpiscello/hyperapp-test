afddf3f78bcd093cc0eca93275baa541cd33ef33
Lucas Cosme <lpiscello@gmail.com>
2018-01-04 19:04:28 -0200

    Splitting css from js

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const combineLoaders = require('webpack-combine-loaders'); // eslint-disable-line import/no-extraneous-dependencies
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle-[hash].js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     contentBase: './dist',
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Awesome app!',
-    inject: 'body',
-    hash: true,
-  })],
+  plugins: [
+    new CleanWebpackPlugin('dist'),
+    new ExtractTextPlugin('styles-[hash].css'),
+    new HtmlWebpackPlugin({
+      title: 'Awesome app!',
+      inject: 'body',
+      hash: true,
+    }),
+  ],
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-      },
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
@@ -31,6 +34,16 @@ module.exports = {
             plugins: ['transform-object-rest-spread', ['transform-react-jsx', { pragma: 'h' }]],
           },
         },
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(combineLoaders([{
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]',
+          },
+        }])),
       },
     ],
   },
